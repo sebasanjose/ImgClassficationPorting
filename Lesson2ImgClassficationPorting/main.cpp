6d07ae0edeef5ade4b35a42c215e5a2561f56236
sebasanjose <sebaenusa@gmail.com>
2024-10-16 11:27:06 -0700

    Initial Commit

---
+++ Lesson2ImgClassficationPorting/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lesson2ImgClassficationPorting
+//
+//  Created by Sebastian Juarez on 10/16/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
